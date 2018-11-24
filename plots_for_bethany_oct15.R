5e868f4a5a8da2ee6ca4492337c8b5dbb45933e6
‘admahood’ <admahood@gmail.com>
2018-11-23 17:13:15 -0700

    l

--- plots_for_bethany_oct15.R
+++ plots_for_bethany_oct15.R
@@ -4,18 +4,23 @@ lapply(libs, library, character.only = TRUE)
 
 source("lmms.R")
 
-three_year <- read.csv("data/all_3_years.csv")
-ff <- read.csv("data/BRTE_mass_x_plot_pt9_m2_per_sample_2016.csv")
-js <- read.csv("data/BRTE_cover_mass_2pt2_m2_per_sample_2016.csv")
+idaho_2018 <- read.csv("data/all_3_years.csv") %>%
+  filter(region == "idaho", year == "2018") %>%
+  mutate(mass_gm2 = mass_g*10)
+
+ff <- read.csv("data/BRTE_mass_x_plot_pt9_m2_per_sample_2016.csv") %>%
+  mutate(mass_gm2 = BRTE_mass/0.9)
+js <- read.csv("data/BRTE_cover_mass_2pt2_m2_per_sample_2016.csv") %>%
+  mutate(mass_gm2 = Mass/2.2)
 bm <- read.csv("data/bm_site_data_w_biomass.csv")
 
-s1<-summary(lm(BRTE_mass~BRTE_cover,ff))
-s2<-summary(lm(Mass~BRTE,js))
+s1<-summary(lm(mass_gm2~BRTE_cover,ff))
+s2<-summary(lm(mass_gm2~BRTE,js))
 s3<-summary(lm(a_grass_gm2~a_grass_cover,bm))
-s4<-summary(lm(log(mass_g)~cover_pct,beautiful_clean_thing))
+s4<-summary(lm(mass_gm2~cover_pct,idaho_2018))
 #s4<-summary(lm(log(mass_g)~cover_pct*observers +max_ht_cm,beautiful_clean_thing))
 
-p1 <- ggplot(ff, aes(x=BRTE_cover, y=BRTE_mass)) +
+p1 <- ggplot(ff, aes(x=BRTE_cover, y=mass_gm2)) +
   geom_point() +
   ggtitle(paste("Cover: 9 m2, Mass 0.9 m2. R2 = ", round(s1$r.squared,2))) +
   ylab("Cheatgrass Biomass (g)") +
@@ -23,7 +28,7 @@ p1 <- ggplot(ff, aes(x=BRTE_cover, y=BRTE_mass)) +
   theme_bw() +
   theme(plot.title = element_text(size = 12))
 
-p2 <- ggplot(js, aes(x=BRTE, y = Mass)) +
+p2 <- ggplot(js, aes(x=BRTE, y = mass_gm2)) +
   geom_point() +
   ggtitle(paste("Cover: 2.2 m2, Mass 2.2 m2. R2 = ", round(s2$r.squared,2))) +
   ylab("Cheatgrass Biomass (g)") +
@@ -40,7 +45,7 @@ p3 <- ggplot(bm, aes(x=a_grass_cover, y=a_grass_gm2)) +
   theme_bw() +
   theme(plot.title = element_text(size = 12))
 
-p4 <- ggplot(beautiful_clean_thing, aes(x=cover_pct, y=sqrt(mass_g))) +
+p4 <- ggplot(beautiful_clean_thing, aes(x=cover_pct, y=mass_gm2)) +
   geom_point() +
   #geom_smooth(method="lm") +
   #geom_line(aes(y=preds))+
@@ -54,4 +59,4 @@ ggarrange(p1,p2,p3,p4)
 ggsave("panel.png",limitsize = FALSE, width = 7.5, height = 6)
 
 
-summary(lm(mass_g~poly(cover_pct,2), data = dplyr::filter(beautiful_clean_thing, year == "2018" & region == "idaho")))
+summary(lm(mass_gm2~poly(cover_pct,2), data = idaho_2018))
