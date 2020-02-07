d389292d208bdc12d090018f5c2ceb3c167ac262
‘admahood’ <admahood@gmail.com>
2020-02-07 13:28:26 -0700

    l

--- R/c_figs_2-4_lmms.R
+++ R/c_figs_2-4_lmms.R
@@ -11,79 +11,133 @@ source("R/a_data_prep.R")
 theme_set(theme_classic())
 
 # figure 2 ---------------------------------------------------------------------
-f2_data <- rbind(ff %>% dplyr::select(Plot, cover_pct, mass_gm2)%>%
-                   na.omit()%>%
-                   mutate(study = "ff",
-                          preds = predict(lm(mass_gm2~cover_pct,.)),
-                          r2 = summary(lm(mass_gm2~cover_pct,.))$r.squared),
+f2_data <- rbind(
+  # ff %>% dplyr::select(Plot, cover_pct, mass_gm2)%>%
+  #                  na.omit()%>%
+  #                  mutate(study = "ff",
+  #                         preds = predict(lm(mass_gm2~cover_pct,.)),
+  #                         r2 = summary(lm(mass_gm2~cover_pct,.))$r.squared),
                  js %>% dplyr::select(Plot, cover_pct, mass_gm2)%>%
                    na.omit()%>%
-                   mutate(study = "js",
-                          preds = predict(lm(mass_gm2~cover_pct,.)),
-                          r2 = summary(lm(mass_gm2~cover_pct,.))$r.squared), 
+                   mutate(study = "js"), 
                  bm17%>% dplyr::select(Plot=plot, cover_pct, mass_gm2)%>%
                    na.omit()%>%
-                   mutate(study = "bm17",
-                          preds = predict(lm(mass_gm2~cover_pct,.)),
-                          r2 = summary(lm(mass_gm2~cover_pct,.))$r.squared),
+                   mutate(study = "bm17"),
                  bm18%>% dplyr::select(Plot=plot, cover_pct, mass_gm2)%>%
                    na.omit()%>%
-                   mutate(study = "bm18",
-                          preds = predict(lm(mass_gm2~cover_pct,.)),
-                          r2 = summary(lm(mass_gm2~cover_pct,.))$r.squared),
+                   mutate(study = "bm18"),
                  bm19%>% dplyr::select(Plot=plot, cover_pct, mass_gm2)%>%
                    na.omit()%>%
-                   mutate(study = "bm19",
-                          preds = predict(lm(mass_gm2~cover_pct,.)),
-                          r2 = summary(lm(mass_gm2~cover_pct,.))$r.squared))
-
-# add in regression equation
-ggplot(f2_data, aes(x=cover_pct, y=mass_gm2)) +
+                   mutate(study = "bm19"))
+f2_data %>%
+  nest(-study) %>% 
+  mutate(model = map(data,~lm(mass_gm2~0+cover_pct, data = .x)),
+         adj.r.squared = map_dbl(model, ~ signif(summary(.x)$r.squared, 3)),
+         slope = map_dbl(model, ~signif(summary(.x)$coefficients[1],3)),
+         p=map_dbl(model, ~ signif(summary(.x)$coefficients[4],3)))  %>% 
+  select(-data, -model) %>% 
+  left_join(f2_data) %>% 
+  mutate(p = ifelse(p < 0.05, "p < 0.05", "p > 0.05"))%>%
+  ggplot(aes(x=cover_pct, y=mass_gm2)) +
   geom_point() +
-  geom_line(aes(y=preds)) +
+  ylab(expression(Mass~(g~m^-2))) +
+  xlab("Percent Cover") +
+  geom_smooth(method="lm", se=FALSE) +
   facet_wrap(~study, ncol = 1) +
-  geom_label(aes(x=0,y=200, label = paste("R2: ",round(r2,2))),
-             hjust = "left")+
+  geom_text(aes(0, 200, label = paste("R2 = ",adj.r.squared, 
+                                     "\n",p,
+                                     "\ny = 0 + ", slope,"x")), 
+            hjust="left", vjust="top")+
   ggsave("figures/figure_2_panel.png", height = 10, width=4)
 
-
 # figure 3 ---------------------------------------------------------------------
-all_ps <- filter(all, study != "id18", study != "c19", study != "w19")
+mod_f3 <- lm(mass_gm2 ~ 0 + cover_pct, f2_data)
 
-x <- lmer(mass_gm2 ~ 0 + cover_pct +(1| study), na.omit(all_ps))
-sx <- summary(x)
-rx <- r.squaredLR(x)
+preds <- predict(mod_f3, interval = "confidence", level=.95) %>%
+  as_tibble 
+mm <- mod_f3$coefficients %>% round(2) %>% as.numeric
 
-# add in regression equation
+f2_data<- f2_data %>%
+  cbind(preds) 
+f2_data$slope=mm
 
-ggplot(na.omit(all_ps), aes(x=cover_pct, y=mass_gm2, color = study)) +
+ggplot(f2_data, aes(x=cover_pct, y=mass_gm2, color = study)) +
   geom_point() +
-  geom_abline(slope = sx$coefficients[1], color = "red", lwd=1) +
-  ggtitle(paste("All studies. pseudo R2 = ", signif(rx[1],2))) +
-  theme_pubr() +
+  geom_line(aes(y=fit), color="red")+
+  geom_line(aes(y=upr), color="red", lty=2)+
+  geom_line(aes(y=lwr), color="red", lty=2)+
+  ylab(expression(Mass~(g~m^-2))) +
+  xlab("Percent Cover") +
+  geom_text(aes(0, 180, label = paste0("y = ", slope, "x + 0")),
+            parse=FALSE, hjust="left", color="black")+
+  geom_text(aes(0, 170, label = paste(expression(R^2~0.89))),
+            parse=TRUE,hjust="left", color="black")+
   theme(plot.title = element_text(size = 12))  +
-  theme(legend.justification=c(0,1), legend.position=c(0,1),
+  theme(legend.justification=c(1,0), legend.position=c(1,0),
         legend.background = element_rect(fill = 'transparent'))+
   ggsave("figures/figure_3_lmm_line.png",
-         width=6,height=6, limitsize = FALSE);p7
+         width=6,height=6, limitsize = FALSE)
 
-# figure 4 ---------------------------------------------------------------------
-w <- lm(mass_gm2~0+cover_pct, data = na.omit(all_ps))
-sw <- summary(w)
-p6 <- ggplot(na.omit(all_ps), aes(x=cover_pct, y=mass_gm2, color = study)) +
-  # geom_abline(slope=1, intercept = 0)+
-  geom_point() +
-  #geom_smooth(method="lm") +
-  geom_line(aes(y=predict(w)), color = "red")+
-  ggtitle(paste("All Studies aggregated. R2 = ", round(sw$r.squared,2), 
-                ", slope = ",round(as.numeric(w$coefficients),2),
-                "\nLinear Model with No Interactions")) +
-  theme_bw() +
-  theme(plot.title = element_text(size = 12)) +
-  theme(legend.justification=c(0,1), legend.position=c(0,1),
-        legend.background = element_rect(fill = 'transparent'))+
-  ggsave("figures/f4_whole_hog.png", width=6,height=6,limitsize = FALSE)
 
+# figure for 1m2 plots----------------------------------------------------------
+bct <- beautiful_clean_thing %>%
+  mutate(study = str_to_title(paste0(region, " ", year)))%>% 
+  filter(study != "Utah 2016") %>%
+  rbind(beautiful_clean_thing %>% mutate(study = "All Together"))
+
+bct %>%
+    nest(-study) %>% 
+    mutate(model = map(data,~lm(mass_g~0+cover_pct, data = .x)),
+           adj.r.squared = map_dbl(model, ~ signif(summary(.x)$r.squared, 2)),
+           slope = map_dbl(model, ~signif(summary(.x)$coefficients[1],2)),
+           p=map_dbl(model, ~ signif(summary(.x)$coefficients[4],3)))  %>% 
+    select(-data, -model) %>% 
+    left_join(bct) %>% 
+    mutate(p = ifelse(p < 0.05, "p < 0.05", "p > 0.05"))%>%
+    ggplot(aes(x=cover_pct, y=mass_g)) +
+    geom_point() +
+    geom_smooth(se = FALSE, method = "lm") +
+    ylab(expression(Mass~(g~m^-2))) +
+    xlab("Percent Cover") +
+    facet_wrap(~study) +
+    geom_text(aes(3, 40, label = paste("R2 = ",adj.r.squared, 
+                                       "\n",p,
+                                       "\ny = 0 + ", slope,"x")), 
+              hjust="left") +
+  ggsave("figures/one_meter_sq_plots.png", height = 10, width = 10)
+
+# subplot vs plot level
+
+bm_agg <- bm_sp %>%
+  mutate(plot = str_sub(plot, 1,5))%>%
+  group_by(plot) %>%
+  summarise(cover_pct = sum(cover_pct)/5,
+            mass_gm2 = sum(mass_gm2)/5,
+            subplot = "plot",
+            scale = "plot") %>%
+  ungroup() %>%
+  rbind(bm_sp %>% 
+          dplyr::select(-biomass,-type) %>%
+          mutate(scale = "subplot"))
+
+bm_agg %>%
+  nest(-scale) %>% 
+  mutate(model = map(data,~lm(mass_gm2~0+cover_pct, data = .x)),
+         adj.r.squared = map_dbl(model, ~ signif(summary(.x)$r.squared, 2)),
+         slope = map_dbl(model, ~signif(summary(.x)$coefficients[1],2)),
+         p=map_dbl(model, ~ signif(summary(.x)$coefficients[4],3)))  %>% 
+  select(-data, -model) %>% 
+  left_join(bm_agg) %>% 
+  mutate(p = ifelse(p < 0.05, "p < 0.05", "p > 0.05"))%>%
+  ggplot(aes(x=cover_pct, y=mass_gm2)) +
+    geom_point() +
+    geom_smooth(method="lm", se=F)+
+    geom_text(aes(0, 150, label = paste("R2 = ",adj.r.squared, 
+                                       "\n",p,
+                                       "\ny = 0 + ", slope,"x")), 
+              hjust="left")+
+    facet_wrap(~scale) +
+  ggsave("figures/plot_vs_subplot.png", height =5, width=6)
 # figure s1 --------------------------------------------------------------------
 
 id_wgb_2018 <- read.csv("data/all_3_years.csv") %>%
